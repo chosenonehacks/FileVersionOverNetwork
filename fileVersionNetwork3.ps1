720abb65d615c49555a49f54e181119045bbd32a
hal9k2 <hal9k2@hotmail.com>
2016-07-18 12:58:38 +0100

    Initit

---
+++ fileVersionNetwork3.ps1
@@ -0,0 +1,13 @@
+﻿ 
+$computernames = Get-Content C:\test\Computers2.csv 
+foreach ($computer in $computernames) {  
+
+$filepath = Test-Path "\\$computer\c$\Program Files (x86)\TIS\eFlow 5\Bin2\" 
+ 
+if ($filepath -eq "True") { 
+
+    Get-ChildItem -Path "\\$computer\c$\Program Files (x86)\TIS\eFlow 5\Bin2\" -Filter *.* | Select-Object Name, @{Name="Version";Expression={$_.VersionInfo.FileVersion}}, LastWriteTime, Length  > "C:\test\$($computer)_Results.txt"
+
+    }
+} 
+     
